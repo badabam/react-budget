a565362743d3cd9e8266d66158f6b4511c91d3d8
Jeremias Erbs <jerry@badabam.de>
2018-06-01 09:21:28 +0200

    Add reselect

--- src/containers/BudgetHeaderView.js
+++ src/containers/BudgetHeaderView.js
@@ -1,8 +1,15 @@
 import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
 import BudgetHeader from '../components/BudgetHeader'
 
+const getSpendings = state => state.spendings
+const restOfBudget = createSelector(getSpendings, spendings =>
+  spendings.reduce((acc, curr) => acc - curr.amount, 1000)
+)
+
 const mapStateToProps = state => ({
-  restOfBudget: state.spendings.reduce((acc, curr) => acc - curr.amount, 1000),
+  restOfBudget: restOfBudget(state),
 })
 
 export default connect(mapStateToProps)(BudgetHeader)
