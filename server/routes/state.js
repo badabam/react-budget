37636306ed6e0f709e8292f188af879a2280bcbc
Jeremias Erbs <jerry@badabam.de>
2018-06-19 16:28:55 +0200

    Cleanup

--- server/routes/state.js
+++ server/routes/state.js
@@ -1,7 +1,7 @@
-var express = require('express')
-var router = express.Router()
-var data = require('../data.json')
-var fs = require('fs')
+const express = require('express')
+const router = express.Router()
+const data = require('../data.json')
+const fs = require('fs')
 
 router.get('/', function(req, res) {
   res.json(data)
